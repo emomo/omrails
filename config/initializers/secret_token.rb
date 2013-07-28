a1d505e3b2d0cd988bf4730bc09418c3828999f8
erica <erica.moen@gmail.com>
2013-07-28 16:37:04 -0500

    Add pg gem for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'c56c6c9179c76e24755869414db475877689f6b79e439089108dd717d9c003d487ce4bce0927e314e05b7ac6d0710ebd1e22c7e49f6c05a1eb8d9c2d6d444872'
